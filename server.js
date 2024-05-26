07751dd165dddea6608827adee17d09d2f6d7353
Ryan <ryanwong935@gmail.com>
2024-05-26 10:47:36 -0400

    CSS file shows up in HTML now through express

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 console.log("I don't wanna do it.");
 
 //Import the Express JS module. We're going to use Express JS framework for the back-end.
-const express = require("express");
-const path = require('path');
+const EXPRESS = require("express");
+const PATH = require('path');
 //Creates the Express application.
-const APP = express();
+const APP = EXPRESS();
 //Port we'll use for HTTP.
 const PORT = 3000;
+
+//Serves static files such as CSS.
+APP.use(EXPRESS.static(__dirname));
 
 /*  Whenever there is an HTTP get request, this callback function will be invoked.
     The function takes in 2 arguments. "req" is the request object. "res" is the response object.
     The send function will send a HTTP response.
 */
 APP.get("/", function(req, res)
-{cd
-    //console.log(path);
-    res.sendFile(path.join(__dirname, '/index.html'));
-    //res.sendFile('../../FRONTEND/index.html');
+{
+    res.sendFile(PATH.join(__dirname, '/index.html'));
 });
 
-APP.listen(PORT, function()
-{
-    console.log('Listening on port: ${PORT}'); 
-});
+// APP.listen(PORT, function()
+// {
+//     console.log('Listening on port: ${PORT}'); 
+// });
