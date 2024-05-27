e07a0a5bdbea61b97b8f834cdfb4c75116dd13a7
Ryan <ryanwong935@gmail.com>
2024-05-26 22:26:35 -0400

    Error detection added for calculating grades.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function read_grades()
+function calculate_grades()
 {
     //This is the form on the page where the user can input their assignment name, grades, and weights.
     const FORM = document.getElementById("grade-form");
@@ -17,15 +17,28 @@ function read_grades()
         if(weight != "") are_weights_empty = false;
         //Make an alert if a grade is full but weight is empty, they can't do that. and vice versa.
         if(grade == "" && weight == "") continue;
-        average += grade * (weight / 10);
-        
+        if(isNaN(grade) || isNaN(weight))
+        {
+            alert("One or more grades and/or weights that you've entered is not a valid number!");
+            return;
+        }
+        if(are_grades_empty || are_weights_empty) 
+        {
+            alert("Please make sure each set of grade has a respective weight and vice versa!");
+            return;
+        }
+        average += grade * (weight / 100);
+        //After every row of grades and weights we read, reset the empty values so we can detect incompleteness in the form.
+        are_grades_empty = true;
+        are_weights_empty = true;
     }
+
+    console.log(average);
 }
 
 document.getElementById("calculate-button").onclick = function()
 {   
-    //console.log("ratio");
-    read_grades();
+    calculate_grades();
 };
 
 function only_allow_numbers(event)
