113b746520dc2fa9cafac90d6716bdf6fa2644bc
Ornie <orniepay@buffalo.edu>
2024-06-07 21:09:50 -0400

    Grade calculator project app deployment on Digital Ocean (more reliable, comfortable using and better than hostinger, bluehost, etc. imo), WORKING COMMAND THAT RUNS ALL/BOTH SERVERS CONCURRENTLY : THE CLIENT <npm start> AND SERVER <npm run dev> FOR BACKEND SERVER AND FRONTEND SERVER <node server.js> ALL IN ONE COMMAND!!! : <npm run start:servers>

--- server.js
+++ server.js
@@ -1,45 +1,39 @@
-function init_server()
-{
+function init_server() {
     console.log("the server is running gals!");
 
-    //Import the Express JS module. We're going to use Express JS framework for the back-end.
+    // Import the Express JS module. We're going to use Express JS framework for the back-end.
     const EXPRESS = require("express");
     const PATH = require('path');
-    //Creates the Express application.
+    // Creates the Express application.
     const APP = EXPRESS();
-    //Port we'll use for HTTP.
+    // Port we'll use for HTTP.
     const PORT = 8080;
 
-    //Serves static files such as CSS.
+    // Serves static files such as CSS.
     APP.use(EXPRESS.static(__dirname));
 
     /*  Whenever there is an HTTP get request, this callback function will be invoked.
         The function takes in 2 arguments. "req" is the request object. "res" is the response object.
         The send function will send a HTTP response.
     */
-    APP.get("/", function(req, res)
-    {
+    APP.get("/", function (req, res) {
         res.sendFile(PATH.join(__dirname, '/grade-calculator.html'));
     });
 
-    APP.get("/final-grade-calculator", function(req, res)
-    {
+    APP.get("/final-grade-calculator", function (req, res) {
         res.sendFile(PATH.join(__dirname, '/projected-final-grade-calculator.html'));
     });
 
-    APP.get("/GPA-calculator", function(req, res)
-    {
+    APP.get("/GPA-calculator", function (req, res) {
         res.sendFile(PATH.join(__dirname, "/gpa-calculator.html"));
     });
 
-    APP.get("/login", function(req, res)
-    {
+    APP.get("/login", function (req, res) {
         res.sendFile(PATH.join(__dirname, '/login.html'));
     });
-    
-    APP.listen(PORT, function()
-    {
-        console.log(`Listening on port: ${PORT}`); 
+
+    APP.listen(PORT, function () {
+        console.log(`Listening on port: ${PORT}`);
     });
 }
 
